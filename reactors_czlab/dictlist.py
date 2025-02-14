afa7b7888c79182b73a8acb07ac4592874b49cd6
asaldivarg93 <asaldivarg93@gmail.com>
2025-02-13 16:36:45 -0800

    set up of the project

---
+++ reactors_czlab/dictlist.py
@@ -0,0 +1,28 @@
+"""Define the DictList class."""
+
+
+class Dictlist(list):
+    """Define a combined dict and list.
+
+    This object behaves like a list, but has the O(1) speed
+    benefits of a dict when looking up elements by their id.
+
+    """
+
+    def __init__(self, iterable: list) -> None:
+        """Instance of DictList.
+
+        Input
+        -----
+        iterable : list
+            A list containing the objects needed in the DictList
+        """
+        list.extend(self, iterable)
+        self._dict = self._generate_index()
+
+    def _generate_index(self) -> dict:
+        return {obj.id: k for k, obj in enumerate(self)}
+
+    def get_by_id(self, identifier: str) -> None:
+        """Find a member of DictList by the id."""
+        return list.__getitem__(self, self._dict[identifier])
